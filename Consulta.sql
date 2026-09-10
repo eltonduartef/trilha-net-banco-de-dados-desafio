6f1f72571e8e807abd5cb6ee6541f821d8f6ca75
Elton Duarte <eltonduartef777@gmail.com>
2026-09-09 22:27:52 -0300

    Adicionado o desafio de sql

--- Consulta.sql
+++ Consulta.sql
@@ -1,43 +1,35 @@
 -- 1 - Buscar o nome e ano dos filmes
-select Nome, Ano from Filmes;
+select Nome, Ano from filmes;
 
--- 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
-select Nome, Ano from Filmes order by Ano Asc;
+-- 2 - Buscar o nome e ano dos filmes, ordenados por ordações por pelo ano crescente
+select Nome, Ano from filmes order by ano asc
 
 -- 3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
-select Nome, Ano, Duracao from Filmes where Nome = 'de volta para o futuro';
+select Nome, Ano, Duracao from filmes where Nome = 'De Volta para o Futuro'
 
 -- 4 - Buscar os filmes lançados em 1997
-select * from Filmes where Ano = 1997;
+select * from filmes where Ano = 1997
 
 -- 5 - Buscar os filmes lançados APÓS o ano 2000
-select * from Filmes where Ano > 2000;
+select * from filmes where Ano > 2000
 
 -- 6 - Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
-select * from Filmes where Duracao > 100 and Duracao < 150 order by Duracao Asc;
+select * from filmes where Duracao > 100 and Duracao < 150 order by Duracao asc
 
 -- 7 - Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
-SELECT 
-    Ano,
-    COUNT(*) AS quantidade
-FROM 
-    filmes
-GROUP BY 
-    Ano
-ORDER BY 
-    quantidade DESC;
+select Ano, COUNT(*) as Quantidade from filmes group by ano order by Quantidade desc
 
 -- 8 - Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
-select PrimeiroNome, UltimoNome, Genero from Atores where Genero = 'M';
+select PrimeiroNome, UltimoNome, Genero from atores where Genero = 'M'
 
 -- 9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
-select PrimeiroNome, UltimoNome, Genero from Atores where Genero = 'F' order by PrimeiroNome Asc;
+select PrimeiroNome, UltimoNome, Genero from atores where Genero = 'F' order by primeironome asc
 
 -- 10 - Buscar o nome do filme e o gênero
 select Nome, Genero from Filmes f, FilmesGenero fg, Generos g where g.Id = fg.IdGenero;
 
 -- 11 - Buscar o nome do filme e o gênero do tipo "Mistério"
-select Nome, Genero from Filmes f, FilmesGenero fg, Generos g where g.Id = fg.IdGenero and g.Genero = 'Mistério';
+select Nome, Genero from Filmes f, FilmesGenero fg, Generos g where g.Id = fg.IdGenero and g.genero = 'Mistério'
 
 -- 12 - Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
-select Nome, PrimeiroNome, UltimoNome, Papel from Filmes f, Atores a, ElencoFilme ef where a.Id = ef.Id;
+select Nome, PrimeiroNome, UltimoNome, Papel from Filmes f, Atores a, ElencoFilme ef where a.Id = ef.IdAtor;
